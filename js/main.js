19dc0d5e65d7abbbc913990f405f2c9fe3d329c9
ouchi <misaki.ouchi17@gmail.com>
2023-08-08 17:34:48 +0900

    タブの修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 Vue.createApp({
   data: function () {
     return {
-      sortOrder: "1",
+      count1: 0,
+      count2: 0,
+      count3: 0,
+      count4: 0,
+      count5: 0,
       list: []
     };
   },
@@ -12,22 +16,85 @@ Vue.createApp({
   },
   computed: {
     orderedList: function () {
-      // IDを選択
-      if (this.sortOrder === 1) {
-      }
-      // 昇順を選択
-      else if (this.sortOrder === 2) {
-        this.list.sort((a, b) => {
-          return a.name.localeCompare(b.name, "ja");
+      let newList = this.list.slice();
+      // ID名クリック
+      if (this.count1 % 2 === 0) {
+        newList.sort((a, b) => {
+          return a.id - b.id;
+        });
+      } else if (this.count1 % 2 !== 0) {
+        newList.sort((a, b) => {
+          return b.id - a.id;
         });
       }
-      // 降順を選択
-      else if (this.sortOrder === 3) {
-        this.list.sort((a, b) => {
+      // 名前クリック
+      if (this.count2 % 2 !== 0) {
+        newList.sort((a, b) => {
+          return a.name.localeCompare(b.name, "ja");
+        });
+      } else if (this.count2 % 2 === 0 && this.count2 !== 0) {
+        newList.sort((a, b) => {
           return b.name.localeCompare(a.name, "ja");
         });
       }
-      return this.list;
+      // 会社名クリック
+      if (this.count3 % 2 !== 0) {
+        newList.sort((a, b) => {
+          return a.company.localeCompare(b.company, "ja");
+        });
+      } else if (this.count3 % 2 === 0 && this.count3 !== 0) {
+        newList.sort((a, b) => {
+          return b.company.localeCompare(a.company, "ja");
+        });
+      }
+      // 部署クリック
+      if (this.count4 % 2 !== 0) {
+        newList.sort((a, b) => {
+          return a.division.localeCompare(b.division, "ja");
+        });
+      } else if (this.count4 % 2 === 0 && this.count4 !== 0) {
+        newList.sort((a, b) => {
+          return b.division.localeCompare(a.division, "ja");
+        });
+      }
+      // 役職クリック
+      if (this.count5 % 2 !== 0) {
+        newList.sort((a, b) => {
+          return a.title.localeCompare(b.title, "ja");
+        });
+      } else if (this.count5 % 2 === 0 && this.count5 !== 0) {
+        newList.sort((a, b) => {
+          return b.title.localeCompare(a.title, "ja");
+        });
+      }
+      return newList;
+    }
+  },
+  methods: {
+    sortId: function () {
+      this.count1++;
+    },
+    sortName: function () {
+      this.count2++;
+    },
+    sortCom: function () {
+      this.count3++;
+    },
+    sortDiv: function () {
+      this.count4++;
+    },
+    sortTitle: function () {
+      this.count5++;
     },
   },
+  // sortList: function (key) {
+  //   this.list.sort((a, b) => {
+  //     return a.key.localeCompare(b.key, "ja");
+  //   });
+  // },
+  // sortDown: function (key) {
+  //   this.list.sort((a, b) => {
+  //     return b.key.localeCompare(a.key, "ja");
+  //   });
+  // },
 }).mount("#app");
